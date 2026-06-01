df2cadd0b0e746040d11e4849ecf8e1a55ad307a
Tadeu Neto <tadeuneto@MacBook-Pro-de-Tadeu.local>
2026-06-01 15:31:05 -0300

    Exercício 26: Adiciona validadores para os campos telefone e email

--- backend/src/v2tec/intranet/content/area.py
+++ backend/src/v2tec/intranet/content/area.py
@@ -2,6 +2,7 @@ from plone.dexterity.content import Container
 from plone.schema.email import Email
 from plone.supermodel import model
 from v2tec.intranet import _
+from v2tec.intranet.utils import validadores
 from zope import schema
 from zope.interface import implementer
 
@@ -20,12 +21,14 @@ class IArea(model.Schema):
     email = Email(
         title=_("Email"),
         required=True,
+        constraint=validadores.is_valid_email,
     )
 
     telefone = schema.TextLine(
         title=_("Telefone"),
         description=_("Informe o telefone de contato"),
         required=False,
+        constraint=validadores.is_valid_telefone,
     )
 
 
---
+++ backend/tests/utils/test_validadores.py
@@ -0,0 +1,37 @@
+from v2tec.intranet.utils import validadores
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "value,expected",
+    [
+        ["1@v2solucoes.com.br", True],
+        ["foobar@v2solucoes.com.br", True],
+        ["bar-foo@v2solucoes.com.br", True],
+        ["1@v2solucoes.com.br.br", False],
+        ["foobar@v2solucoes.com.br.br", False],
+        ["bar-foo@v2solucoes.com.br.br", False],
+        ["ericof@simplesconsultoria.com.br", False],
+    ],
+)
+def test_is_valid_email(value: str, expected: bool):
+    """Testa a função is_valid_email."""
+    assert validadores.is_valid_email(value) is expected
+
+
+@pytest.mark.parametrize(
+    "value,expected",
+    [
+        ["61999528312", True],
+        ["65-99952.8312", False],
+        ["(61)999528312", False],
+        ["6132104100", True],
+        ["5132104100", True],
+        [" ", False],
+        ["(999)1234566", False],
+    ],
+)
+def test_is_valid_telefone(value: str, expected: bool):
+    """Testa a função test_is_valid_telefone."""
+    assert validadores.is_valid_telefone(value) is expected
